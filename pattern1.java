383900147cd86810ffbea7227e46ca29a953a505
NehaNNagpure <47938465+NehaNNagpure@users.noreply.github.com>
2022-05-02 23:24:09 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class program
+{
+	int iMul=1;
+	public void pattern(int iNo)
+	{
+		for(int iCnt=1;iCnt<=iNo;iCnt++)
+		{
+			System.out.print("*\t");
+		}
+
+		
+	}
+}
+public class pattern1
+{
+	public static void main(String[] args)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		if(sc.hasNextInt())
+		{
+			int no=sc.nextInt();
+			
+			if(no<0)
+			{
+				System.out.println("negative numbers not allowed");
+			}
+			else
+			{
+				program p=new program();
+				p.pattern(no);
+			
+			}
+			
+		}
+		else
+		{
+			System.out.println("Invalid numbers");
+		}
+	}
+}
